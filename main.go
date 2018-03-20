666adad93e1cacb6913aef02ed4fc55f24e04700
Akshay Shah <shah@uber.com>
2018-03-19 22:06:44 -0700

    Add minimal reproduction code

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/akshayjshah/version"
+)
+
+func main() {
+	fmt.Printf("version is %s\n", version.Version)
+}
---
+++ vendor/github.com/akshayjshah/version/version.go
@@ -0,0 +1,5 @@
+// Package version shows off a simple way of using linker flags to inject
+// information into a Go binary.
+package version
+
+var Version = "undefined"
